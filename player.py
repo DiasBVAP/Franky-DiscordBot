fb0486f0a924e9ed4243f686600c48465770ba11
AsaiOgawa <76762506+AsaiOgawa@users.noreply.github.com>
2021-09-26 18:09:33 -0300

    Updated player.py to ignore mixer exceptions

--- player.py
+++ player.py
@@ -2,17 +2,29 @@ from pygame import mixer
 import time
 
 def play_music(fileName: str):
-    mixer.init(devicename='CABLE Input (VB-Audio Virtual Cable)')
-    mixer.music.load(fileName)
-    mixer.music.set_volume(1.0)
-    mixer.music.play()
+    try:
+        mixer.init(devicename='CABLE Input (VB-Audio Virtual Cable)')
+        mixer.music.load(fileName)
+        mixer.music.set_volume(1.0)
+        mixer.music.play()
+    except:
+        pass
 
 def pause_music():
-    mixer.music.pause()
+    try:
+        mixer.music.pause()
+    except:
+        pass
 
 def stop_music():
-    mixer.music.stop()
-    mixer.quit()
+    try:
+        mixer.music.stop()
+        mixer.quit()
+    except:
+        pass
 
 def unpause_music():
-    mixer.music.unpause()
+    try:
+        mixer.music.unpause()
+    except:
+        pass
