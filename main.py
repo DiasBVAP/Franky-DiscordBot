608d0433f2f940ebcbc1e70e20bcbe9c108793da
AsaiOgawa <76762506+AsaiOgawa@users.noreply.github.com>
2021-09-26 15:46:42 -0300

    Added option to pause, stop, resume music in main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ PLAY = '!!play'
 PAUSE = '!!pause'
 STOP = '!!stop'
 
-if __name__ == "__main__":
+def main():
     q = queue.Queue(1)
     message = ''
 
@@ -22,14 +22,25 @@ if __name__ == "__main__":
         if q.full():
             message = q.get()
             if PLAY in message:
+                print('got it')
                 if 'youtube.com' in message:
                     linkIndex = message.index('youtube')
                     link = message[linkIndex:]
                     downloader.download_music(link)
                     player.play_music('teste.wav')
+                elif len(message) == len(PLAY):
+                    player.unpause_music()
                 else:
                     searchIndex = len(PLAY)
                     searchTerm = message[searchIndex:]
                     link = searcher.search_video(searchTerm)
                     downloader.download_music(link)
                     player.play_music('teste.wav')
+            elif PAUSE in message:
+                player.pause_music()
+            elif STOP in message:
+                player.stop_music()
+
+
+if __name__ == "__main__":
+    main()
