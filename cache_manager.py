f1eb1020b017c0166450e55790a685e0aac59cd1
AsaiOgawa <76762506+AsaiOgawa@users.noreply.github.com>
2021-09-29 19:59:22 -0300

    Add cache_manager

---
+++ cache_manager.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+
+def clean_cache() -> None:
+    load_dotenv()
+    MAX_CACHE_SIZE = int(os.environ.get('MAX_CACHE_SIZE'))
+
+    cache_size = get_folder_size('cache')
+    if cache_size > MAX_CACHE_SIZE:
+        oldestFile = get_oldest_file('cache')
+        os.remove('cache/' + oldestFile)
+
+def get_folder_size(folder: str) -> float:
+    sizeBytes = 0
+    for file in os.listdir(folder):
+        sizeBytes += os.path.getsize('cache/' + file)
+    sizeMega = sizeBytes / (1024 * 1024)
+    return sizeMega
+
+def get_oldest_file(folder: str) -> None:
+    oldestFile = 'cache/' + os.listdir(folder)[0]
+    for file in os.listdir(folder):
+        if os.path.getctime('cache/' + file) > os.path.getctime(oldestFile):
+            oldestFile = file
+    return oldestFile
+
+clean_cache()
