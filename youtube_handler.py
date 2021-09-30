0cc7f50a04724d686260bce7633a7dbd3ef86aa5
AsaiOgawa <76762506+AsaiOgawa@users.noreply.github.com>
2021-09-29 22:27:34 -0300

    youtube_handler will check if video is already in cache before downloading

--- youtube_handler.py
+++ youtube_handler.py
@@ -2,6 +2,9 @@ from __future__ import unicode_literals
 from requests import get
 import youtube_dl
 
+import cache_manager
+import message_sender
+
 
 def get_video(arg: str) -> str:
     ydl_opts = {
@@ -17,7 +20,17 @@ def get_video(arg: str) -> str:
         try:
             get(arg) 
         except:
-            video = ydl.extract_info(f"ytsearch:{arg}", download=True)['entries'][0]
+            video = ydl.extract_info(f"ytsearch:{arg}", download=False)['entries'][0]
+            if cache_manager.is_in_cache(video['id']):
+                return video
+            else:
+                message_sender.send_message('**Downloading . . .**')
+                ydl.download(['https://www.youtube.com/watch?v=' + video['id']])
         else:
-            video = ydl.extract_info(arg, download=True)
+            video = ydl.extract_info(arg, download=False)
+            if cache_manager.is_in_cache(video['id']):
+                return video
+            else:
+                message_sender.send_message('**Downloading . . .**')
+                ydl.download(['https://www.youtube.com/watch?v=' + video['id']])
     return video
