3140538d3d0904a2240a8cc312b9c1bb72f03b10
AsaiOgawa <76762506+AsaiOgawa@users.noreply.github.com>
2021-10-06 20:10:46 -0300

    Implemente queue system and added few commands

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ PLAY = '!!play'
 PAUSE = '!!pause'
 STOP = '!!stop'
 LOOP = '!!loop'
+SKIP = '!!skip'
+QUEUE = '!!queue'
+DEBUG = '!!debug'
 
 def main():
     q = queue.Queue(1)
@@ -25,37 +28,37 @@ def main():
 
             if PLAY in message:
                 if len(message) == len(PLAY):
-                    if Player.unpause():
-                        message_sender.send_message(f'Resuming **{songName}**')
-                    else:
-                        message_sender.send_message('**No song to play**')
+                    if Player.unpause(): message_sender.send_message(f'Resuming **{songName}**')
                 else:
                     payloadIndex = len(PLAY) + 1
                     payload = message[payloadIndex:]
                     songDict = youtube_handler.get_video(payload)
-                    songName = songDict['title']
-                    songID = songDict['id']
-                    Player.play(f'cache/{songID}.mp3')
-                    message_sender.send_message(f'Playing **{songName}**')
+                    Player.add_to_queue(songDict)
+                    #songName = songDict['title']
+                    #songID = songDict['id']
+                    #Player.play(f'cache/{songID}.mp3')
+                    #message_sender.send_message(f'Playing **{songName}**')
                     cache_manager.clean_cache()
 
             elif PAUSE in message:
-                if Player.pause():
-                    message_sender.send_message(f'Paused **{songName}**')
-                else:
-                    message_sender.send_message('**No song to pause**')
+                if Player.pause(): message_sender.send_message(f'Paused **{songName}**')
 
             elif STOP in message:
-                if Player.stop():
-                    message_sender.send_message('**Track Stopped!**')
-                else:
-                    message_sender.send_message('**No song to stop**')
+                if Player.stop(): message_sender.send_message('**Track Stopped!**')
 
             elif LOOP in message:
-                if Player.loop():
-                    message_sender.send_message(f'Looping **{songName}**')
-                else:
-                    message_sender.send_message('**No song to loop**')
+                if Player.loop(): message_sender.send_message(f'Looping **{songName}**')
+
+            elif SKIP in message:
+                if Player.play_next(): message_sender.send_message(f'Skipped **{songName}**')
+
+            elif QUEUE in message:
+                message_sender.send_message(Player.get_queue())
+
+            elif DEBUG in message:
+                message_sender.send_message(Player.debug())
+            
+        Player.update()
 
 
 if __name__ == "__main__":
