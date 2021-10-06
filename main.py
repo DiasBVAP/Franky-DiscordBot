a548c431de168027837402c5f36171e0b6b2c260
AsaiOgawa <76762506+AsaiOgawa@users.noreply.github.com>
2021-10-05 23:27:32 -0300

    Implemente Player class in main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import threading
 import queue
 
 import youtube_handler
-import player
+from player_class import Player
 import websocket_logger
 import message_sender
 import cache_manager
@@ -26,26 +26,34 @@ def main():
             message = q.get()
             if PLAY in message:
                 if len(message) == len(PLAY):
-                    player.unpause_music()
-                    message_sender.send_message(f'Resuming **{songName}**')
+                    if Player.unpause():
+                        message_sender.send_message(f'Resuming **{songName}**')
+                    else:
+                        message_sender.send_message('No song to play')
                 else:
                     payloadIndex = len(PLAY) + 1
                     payload = message[payloadIndex:]
                     songDict = youtube_handler.get_video(payload)
                     songName = songDict['title']
                     songID = songDict['id']
-                    player.play_music(f'cache/{songID}.mp3')
+                    Player.play(f'cache/{songID}.mp3')
                     message_sender.send_message(f'Playing **{songName}**')
                     cache_manager.clean_cache()
             elif PAUSE in message:
-                player.pause_music()
-                message_sender.send_message(f'Paused **{songName}**')
+                if Player.pause():
+                    message_sender.send_message(f'Paused **{songName}**')
+                else:
+                    message_sender.send_message('No song to pause')
             elif STOP in message:
-                player.stop_music()
-                message_sender.send_message('**Track Stopped!**')
+                if Player.stop():
+                    message_sender.send_message('**Track Stopped!**')
+                else:
+                    message_sender.send_message('No song to stop')
             elif LOOP in message:
-                player.loop_music()
-                message_sender.send_message(f'Looping **{songName}**')
+                if Player.loop():
+                    message_sender.send_message(f'Looping **{songName}**')
+                else:
+                    message_sender.send_message('No song to loop')
 
 
 if __name__ == "__main__":
