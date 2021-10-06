1e53122b26d17e3560ccfdc86fa8318f1193e7b6
AsaiOgawa <76762506+AsaiOgawa@users.noreply.github.com>
2021-10-06 13:54:02 -0300

    Make stuff neater

--- main.py
+++ main.py
@@ -15,21 +15,20 @@ LOOP = '!!loop'
 
 def main():
     q = queue.Queue(1)
-
     t1 = threading.Thread(target=websocket_logger.initialize, args=(q,))
     t1.start()
 
     songName = ''
 
     while True:
-        if q.full():
-            message = q.get()
+        if q.full() and ('!!' in (message := q.get())):
+
             if PLAY in message:
                 if len(message) == len(PLAY):
                     if Player.unpause():
                         message_sender.send_message(f'Resuming **{songName}**')
                     else:
-                        message_sender.send_message('No song to play')
+                        message_sender.send_message('**No song to play**')
                 else:
                     payloadIndex = len(PLAY) + 1
                     payload = message[payloadIndex:]
@@ -39,21 +38,24 @@ def main():
                     Player.play(f'cache/{songID}.mp3')
                     message_sender.send_message(f'Playing **{songName}**')
                     cache_manager.clean_cache()
+
             elif PAUSE in message:
                 if Player.pause():
                     message_sender.send_message(f'Paused **{songName}**')
                 else:
-                    message_sender.send_message('No song to pause')
+                    message_sender.send_message('**No song to pause**')
+
             elif STOP in message:
                 if Player.stop():
                     message_sender.send_message('**Track Stopped!**')
                 else:
-                    message_sender.send_message('No song to stop')
+                    message_sender.send_message('**No song to stop**')
+
             elif LOOP in message:
                 if Player.loop():
                     message_sender.send_message(f'Looping **{songName}**')
                 else:
-                    message_sender.send_message('No song to loop')
+                    message_sender.send_message('**No song to loop**')
 
 
 if __name__ == "__main__":
